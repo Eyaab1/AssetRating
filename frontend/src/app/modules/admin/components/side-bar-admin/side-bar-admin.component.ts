511e5ba36aeedff8e429a087709326934917060c
eyaHwess <eyahwess74@gmail.com>
2025-05-16 11:15:01 +0100

    admin sidebar

--- frontend/src/app/modules/admin/components/side-bar-admin/side-bar-admin.component.ts
+++ frontend/src/app/modules/admin/components/side-bar-admin/side-bar-admin.component.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state
+} from '@angular/animations';
+
 @Component({
   selector: 'app-side-bar-admin',
   standalone: true,
   imports: [RouterModule,CommonModule],
   templateUrl: './side-bar-admin.component.html',
-  styleUrl: './side-bar-admin.component.css'
+  styleUrl: './side-bar-admin.component.css',
+  animations: [
+    trigger('slideSidebar', [
+      state('default', style({ transform: 'translateX(0)', opacity: 1 })),
+      state('asset', style({ transform: 'translateX(0)', opacity: 1 })),
+      transition('default => asset', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('300ms ease-out')
+      ]),
+      transition('asset => default', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('300ms ease-out')
+      ])
+    ])
+  ]
+
 })
 export class SideBarAdminComponent {
   firstNameInitial: string = '';
   fullName: string = '';
   showProfileMenu: boolean = false;
+  isAssetView: boolean = false;
+
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
@@ -30,7 +55,10 @@ export class SideBarAdminComponent {
 
   logout(): void {
     localStorage.removeItem('token');
-    location.href = '/login'; // or use router.navigate(['/login'])
+    location.href = '/login'; 
+  }
+  toggleAssetView(): void {
+    this.isAssetView = !this.isAssetView;
   }
 
 
